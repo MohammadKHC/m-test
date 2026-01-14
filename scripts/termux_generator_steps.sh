8eee3cd4569eb96a3dc92b6bc99bf79de176e274
Mohammed Khaled <mohammedkhc@outlook.com>
2026-01-14 13:12:12 +0200

    Update termux_generator_steps.sh

--- scripts/termux_generator_steps.sh
+++ scripts/termux_generator_steps.sh
@@ -59,42 +59,13 @@ clean_artifacts() {
 
 # Funktion, um Repositories herunterzuladen
 download() {
-    if [[ "$TERMUX_APP_TYPE" == "f-droid" ]]; then
         git clone --depth 1 https://github.com/termux/termux-packages.git               termux-packages-main
-        git clone --depth 1 https://github.com/termux/termux-tasker.git                 termux-apps-main/termux-tasker
-        git clone --depth 1 https://github.com/termux/termux-float.git                  termux-apps-main/termux-float
-        git clone --depth 1 https://github.com/termux/termux-widget.git                 termux-apps-main/termux-widget
-        git clone --depth 1 https://github.com/termux/termux-api.git                    termux-apps-main/termux-api
-        git clone --depth 1 https://github.com/termux/termux-boot.git                   termux-apps-main/termux-boot
-        git clone --depth 1 https://github.com/termux/termux-styling.git                termux-apps-main/termux-styling
-        git clone --depth 1 https://github.com/termux/termux-app.git                    termux-apps-main/termux-app
-        git clone --depth 1 https://github.com/termux/termux-gui.git                    termux-apps-main/termux-gui
-        # special case - for "F-Droid" Termux, it is necessary to move the termux-am-library subfolder of
-        # the termux-am-library repository, which contains its actual code, into the termux-app folder,
-        # where its code needs to be patched and compiled into the main "F-Droid" Termux APK
-        git clone --depth 1 https://github.com/termux/termux-am-library.git             termux-apps-main/termux-am-library
-        mv termux-apps-main/termux-am-library/termux-am-library/                        termux-apps-main/termux-app/termux-am-library
-        rm -rf                                                                          termux-apps-main/termux-am-library/
-    else
-        git clone --depth 1 https://github.com/termux-play-store/termux-packages.git    termux-packages-main
-        git clone --depth 1 https://github.com/termux-play-store/termux-apps.git        termux-apps-main
-    fi
-    git clone --depth 1 --recursive https://github.com/termux/termux-x11.git        termux-apps-main/termux-x11
 }
 
 build_plugin() {
-    pushd "plugins/$TERMUX_GENERATOR_PLUGIN"
-
-    ./gradlew build
-
-    popd
 }
 
 install_plugin() {
-    mkdir -p termux-apps-main/termux-app/src/main/assets/
-    cp -rf "plugins/$TERMUX_GENERATOR_PLUGIN" termux-apps-main/termux-app/src/main/assets/
-    apply_patches "plugins/$TERMUX_GENERATOR_PLUGIN/$TERMUX_APP_TYPE-patches/bootstrap-patches" termux-packages-main
-    apply_patches "plugins/$TERMUX_GENERATOR_PLUGIN/$TERMUX_APP_TYPE-patches/app-patches" termux-apps-main
 }
 
 # Funktion, um Bootstrap-Patches anzuwenden
@@ -111,60 +82,18 @@ patch_bootstraps() {
 
     local bashrc="termux-packages-main/packages/bash/etc-bash.bashrc"
 
-    if [[ -n "$ENABLE_SSH_SERVER" ]]; then
-        cat <<- EOF >> "$bashrc"
-            if [ ! -f "\$HOME/.termux/boot/start-sshd" ]; then
-                mkdir -p "\$HOME/.termux/boot"
-                echo '#!/data/data/$TERMUX_APP__PACKAGE_NAME/files/usr/bin/sh' > "\$HOME/.termux/boot/start-sshd"
-                echo '. /data/data/$TERMUX_APP__PACKAGE_NAME/files/usr/etc/bash.bashrc' >> "\$HOME/.termux/boot/start-sshd"
-                chmod +x "\$HOME/.termux/boot/start-sshd"
-            fi
-            if [ ! -f "\$HOME/.termux_authinfo" ]; then
-                printf '$DEFAULT_PASSWORD\n$DEFAULT_PASSWORD' | passwd
-            fi
-            sshd
-EOF
-    fi
-
     cp -f "$TERMUX_GENERATOR_HOME/scripts/termux_generator_utils.sh" termux-packages-main/scripts/
 }
 
 # Funktion, um die App zu patchen
 patch_apps() {
-    apply_patches "$TERMUX_APP_TYPE-patches/app-patches" termux-apps-main
-
-    if [[ "$TERMUX_APP__PACKAGE_NAME" == "com.termux" ]]; then
-        return
-    fi
-
-    replace_termux_name termux-apps-main "$TERMUX_APP__PACKAGE_NAME"
-
-    migrate_termux_folder_tree termux-apps-main "$TERMUX_APP__PACKAGE_NAME"
 }
 
 build_termux_x11() {
-    pushd termux-apps-main/termux-x11
-
-    ./gradlew assembleDebug
-    ./build_termux_package
-
-    popd
 }
 
 
 move_termux_x11_deb() {
-    pushd termux-apps-main/termux-x11
-
-    if [[ "$TERMUX_APP_TYPE" == "f-droid" ]]; then
-        local termux_x11_dest="$TERMUX_GENERATOR_HOME/termux-packages-main/output"
-    else
-        local termux_x11_dest="$TERMUX_GENERATOR_HOME/termux-packages-main"
-    fi
-
-    mkdir -p "$termux_x11_dest"
-    mv app/build/outputs/apk/debug/*.deb "$termux_x11_dest/termux-x11-nightly_all.deb"
-
-    popd
 }
 
 # Funktion, um Bootstraps zu erstellen
@@ -213,51 +142,12 @@ build_bootstraps() {
 
 # Funktion, um Bootstraps zu kopieren
 move_bootstraps() {
-    if [[ "$TERMUX_APP_TYPE" == "f-droid" ]]; then
-        local app_assets_dir="app/src/main/assets/"
-    else
-        local app_assets_dir="src/main/assets/"
-    fi
-    mkdir -p "termux-apps-main/termux-app/$app_assets_dir"
-    mv termux-packages-main/bootstrap-*.zip "termux-apps-main/termux-app/$app_assets_dir"
 }
 
 # Funktion, um die App zu bauen
 build_apps() {
-    pushd termux-apps-main
-
-    if [[ "$TERMUX_APP_TYPE" == "f-droid" ]]; then
-        pushd termux-app
-            ./gradlew publishReleasePublicationToMavenLocal
-        popd
-        for app in *; do
-            if [[ "$app" == "termux-x11" ]]; then
-                continue
-            fi
-            pushd "$app"
-                ./gradlew assembleDebug
-            popd
-        done
-    else
-        ./gradlew assembleDebug
-    fi
-
-    popd
 }
 
 # Funktion, um die APK zu kopieren
 move_apks() {
-    if [[ "$TERMUX_APP_TYPE" == "f-droid" ]]; then
-        local build_dir="app/build/outputs/apk/debug"
-    else
-        local build_dir="build/outputs/apk/debug"
-    fi
-
-    for apk in termux-apps-main/termux-x11/app/build/outputs/apk/debug/*.apk; do
-        mv "$apk" "$TERMUX_APP__PACKAGE_NAME-$TERMUX_APP_TYPE-$(basename $apk)"
-    done
-
-    for apk in termux-apps-main/*/"$build_dir"/*.apk; do
-        mv "$apk" "$TERMUX_APP__PACKAGE_NAME-$TERMUX_APP_TYPE-$(basename $apk)"
-    done
 }
